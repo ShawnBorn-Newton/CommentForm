191c0e18eeed64da7838eca6172e56e36cc83605
ShawnBorn-Newton <shawn.newton3@gmail.com>
2017-08-22 11:03:42 -0400

    first commit

---
+++ CommentForm/Models/Comment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CommentForm.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int CommentId { get; set; }
+
+        public string Name { get; set; }
+        public string CommentText { get; set; }
+        public int Priority { get; set; }
+
+        [ForeignKey("Procedure")]
+        public int ProcedureId { get; set; }
+        public virtual Procedure Procedure { get; set; }
+    }
+}
